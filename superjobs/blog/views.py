375e3507cc29e0dc2779109f3c7ca9f00a4f7e3f
Copser <ppilipovic84@gmail.com>
2016-12-25 08:04:19 +0100

    removed twilio decorator

--- superjobs/blog/views.py
+++ superjobs/blog/views.py
@@ -1,13 +1,13 @@
 # blog/views.py
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals
+
 from django.shortcuts import render, get_object_or_404, render_to_response
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.template import Context, loader, RequestContext
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-
 
 from .models import Post, ApplyFormModel
 from .forms import ApplyForm
