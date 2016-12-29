885e88d2aed945befdff9fd23dcf74212cf0dde6
Copser <ppilipovic84@gmail.com>
2016-12-29 05:38:30 +0100

    define standard TwilioRest client

--- superjobs/utils/sender_confirmation.py
+++ superjobs/utils/sender_confirmation.py
@@ -2,3 +2,13 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals
 
+from twilio.rest import TwilioRestClient
+from django_twilio.utils import discover_twilio_credentials
+
+
+
+my_user = User.objects.get(pk=USER_ID)
+
+account_id, auth_token = discover_twilio_credentials(my_user)
+
+twilio_client = TwilioRestClient(account_id, auth_token)
